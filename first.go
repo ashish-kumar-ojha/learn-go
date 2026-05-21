e7354055e22222c405c72b557c07a48fbc6ad76b
Ashish <ojhaash247@gmail.com>
2026-05-22 00:55:11 +0530

    adds print, println and printf

--- first.go
+++ first.go
@@ -1,7 +1,13 @@
 package main
 
+// fmt = format package, used for formatted I/O operations, such as printing to the console.
 import "fmt"
 
-func main(){
-	fmt.Println("Hello World");
+func main() {
+	//Simple Print without newline
+	fmt.Print("Hello World")
+	//Println adds a newline at the end
+	fmt.Println("I am learning Go")
+	//Printf allows for formatted strings, similar to C's printf
+	fmt.Printf("The value of Pi is approximately %.2f\n", 3.14159)
 }
